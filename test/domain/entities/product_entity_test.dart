5bc74a167ed2d1c13e1c6596799527aa70f6c52f
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2023-04-04 09:00:28 -0300

    test: updated tests

--- test/domain/entities/product_entity_test.dart
+++ test/domain/entities/product_entity_test.dart
@@ -5,13 +5,13 @@ void main() {
   group('ProductEntity', () {
     test('should create ProductEntity with correct properties', () {
       final product = ProductEntity(
-        id: 1,
+        id: 2,
         name: 'Product 1',
         price: 10.0,
         amount: 5,
       );
 
-      expect(product.id, 1);
+      expect(product.id, 2);
       expect(product.name, 'Product 1');
       expect(product.price, 10.0);
       expect(product.amount, 5);
